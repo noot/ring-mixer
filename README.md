b1e5d8e4ee1a6e0bacdfb427e9691b5fc299d63c
ebinks <elizabethjbinks@gmail.com>
2018-11-01 11:14:53 -0400

    updated README, add compile script

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ to eventually be renamed, once I find a good name :)
 
 
 contracts:
-* RingVerify.sol; this contract tests the ring-verify precompile located at address `0x0000000000000000000000000000000000000009i`. see github.com/noot/go-ethereum
+* RingVerify.sol; this contract tests the ring-verify precompile located at address `0x0000000000000000000000000000000000000009`. see github.com/noot/go-ethereum
 * RingMixer.sol; this contract performs transaction obfuscation using verification of a ring-signed message.
 
 ### dependencies
--- contracts/RingVerify.sol
+++ contracts/RingVerify.sol
@@ -6,14 +6,16 @@ contract RingVerify{
 
     event Verify(bool indexed ok);
 
-    function hash(uint256 _data) public returns (bytes32 _h) {
+    function hash(uint256 _data) public returns (bytes32) {
         address _a = address(0x02);
+
+        bytes32 _h;
 
         assembly {            
             let x := mload(0x40) // get empty storage location
             mstore ( x, _data ) 
 
-            let ret := call (gas, 
+            let ret := call(gas, 
                 _a,
                 0, // no wei value passed to function
                 x, // input
@@ -27,21 +29,27 @@ contract RingVerify{
         }
 
         emit Hash(_h);
+        return _h;
     }
+
+    event Input(bytes indexed _sig, uint256 indexed _len);
 
     function verify(bytes _sig) public returns (bool ok) {
         // precompile for verify located at address 0x09
         address _a = address(0x09);
+        uint256 _len = _sig.length;
+        uint256 _gas = 21000;
+        emit Input(_sig, _len);
 
         assembly {            
             let x := mload(0x40) // get empty storage location
             mstore ( x, _sig ) 
 
-            let ret := call (gas, 
+            let ret := call(_gas, 
                 _a,
                 0, // no wei value passed to function
                 x, // input
-                0x00, // input size
+                _len, // input size
                 x, // output stored at input location, save space
                 0x20 // output size = 32 bytes
             )
