c95fb14364af98996068207467b6949f6e12155b
ebinks <elizabethjbinks@gmail.com>
2018-12-06 08:00:51 -0500

    first implementation of calling bn256 precompiles; upgrade to solidity 0.5.0

--- contracts/RingMixer.sol
+++ contracts/RingMixer.sol
@@ -1,4 +1,6 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.0;
+
+import "./EC.sol";
 
 contract RingMixer {
 	// constant ring size
@@ -55,7 +57,7 @@ contract RingMixer {
 	// after a ring is formed, all the members of the ring must submit a ring-signed transaction where the message
 	// is keccak256(address _to, uint256 _value). this signature is stored in the contract until withdrawals are completed
 	// called by the sender
-	function submit_sig(bytes _sig) public {
+	function submit_sig(bytes memory _sig) public {
 		require(_sig.length == SIGLEN);
 		// todo: add checks to make sure signature was formatted correctly, and that the ring in the signature is in fact 
 		// the ring stored in the contract
@@ -66,7 +68,7 @@ contract RingMixer {
 	// verifies that there was in fact a signature submitted to the contract with a message specifying that _value be sent
 	// to _to.
 	// usually called by the receiver; can actually be called by anyone, assuming they know the _to address and the value.
-	function verify(address _to, uint8 i) public returns (bool ok) {
+	function verify(address payable _to, uint8 i) public returns (bool ok) {
 		bytes32 _msg = keccak256(abi.encodePacked(_to, VAL));
 		bytes32 sig_msg;
 		bytes memory sig = sigs[i];
@@ -93,15 +95,15 @@ contract RingMixer {
 			delete ring[i];
 		}
 		emit RoundFinished();
-	}
+	
 
-	// todo: calls the verify precompile
-	function _verify(bytes32 _msg, bytes _sig) internal returns (bool) {
+}	// todo: calls the verify precompile
+	function _verify(bytes32 _msg, bytes memory _sig) internal returns (bool) {
 		return true;
 	}
 
 	// todo: checks if the point is on the curve
-	function _onCurve(uint256 _x, uint256 _y) internal returns (bool) {
+	function _onCurve(uint256 _x, uint256 _y) pure internal returns (bool) {
 		uint256 sqred = mulmod(_x, _x, ORDER);
 		uint256 cubed = mulmod(sqred, _x, ORDER);
 		return addmod(sqred, B, ORDER) == mulmod(_y, _y, ORDER);
